8c3b88a376273de057bbf3e4a01b0194815f6f8e
Tim Fogarty <timothy.fogo@gmail.com>
2013-11-03 22:04:47 +0000

    Delete everything

--- models.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose")
-, Schema = mongoose.Schema
-, ObjectId = Schema.ObjectId;
-
-var UserSchema = new mongoose.Schema({
-    name: String,
-});
-
-mongoose.model("User", UserSchema);
