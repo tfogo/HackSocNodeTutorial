33a36c59fb60e019b843e7b57a0d8c5d8f4b337a
Tim Fogarty <timothy.fogo@gmail.com>
2013-11-05 22:51:48 +0000

    finishes ch4

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ var express = require('express')
 , app = express()
 , routes = require('./routes');
 
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 app.use(app.router);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', routes.index);
+app.get('/chocolates', routes.chocolates);
+
+var repeat = 4;
+app.get('/open', routes.open(repeat));
 
 server = http.createServer(app);
 server.listen(8000);
--- routes.js
+++ routes.js
@@ -1,3 +1,14 @@
 exports.index = function(req, res){
-    res.sendfile(__dirname + "/views/index.html")
-}
+    res.render('index');
+};
+
+exports.chocolates = function(req, res){
+    var numberOfChocs = 10;
+    res.render('chocs', {numberOfChocs : numberOfChocs});
+};
+
+exports.open = function(repeat){ // Wow. Such closure.
+    return function(req, res){
+	res.render('open', {repeat : repeat});
+    };
+};
