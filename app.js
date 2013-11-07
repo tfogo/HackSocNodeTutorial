e1f6786d88a1d2abee2513d847e85953afbaa6af
Tim Fogarty <timothy.fogo@gmail.com>
2013-11-07 18:31:25 +0000

    Sets up ch5 start

--- app.js
+++ app.js
@@ -14,13 +14,3 @@ app.get('/', routes.index);
 server = http.createServer(app);
 server.listen(8000);
 console.log('Express server started on port %s', server.address().port);
-
-io = socket.listen(server);
-
-io.sockets.on('connection', function(client) {
-    console.log('Client connected...');
-    client.on('messages', function(data){
-    	console.log(data);
-	client.broadcast.emit('new message', data)
-    });
-});
